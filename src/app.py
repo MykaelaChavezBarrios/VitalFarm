1dc891daa0673224899fd9be02ba81bc0b2851d3
MykaelaChavezBarrios <tatyana.chavez@ucsm.edu.pe>
2023-05-07 14:51:51 -0500

    feat:app.py connection to the database

--- src/app.py
+++ src/app.py
@@ -1,8 +1,31 @@
 from flask import Flask
-from src.config import config
+from config import config
 from flask_cors import CORS
-from src.config import config
+from database.db import initialize_db, init_app
+from flask_restful import Api
+from database.db_maker import create_db, drop_db
+from resources.routes import initialize_routes
 
 app = Flask(__name__)
 config(app=app)
 CORS(app=app)
+api = Api(app)
+initialize_db(app=app)
+init_app(app=app)
+drop_db(app=app)
+create_db(app=app)
+initialize_routes(api=api)
+
+@app.route("/")
+@app.route("/api")
+def index():
+    return {
+        "Author": "Mykaela",
+        "message": "Welcome to the API REST from VitalFarm",
+        "routes": [
+            "/",
+        ],
+    }
+
+if __name__ == "__main__":
+    app.run(debug=True)
